0b8f772000933c0c0678b8599b16d2acf5c8d9c4
xusifob <b.malahieude@free.fr>
2015-09-29 15:50:17 +0200

    add choices.json

--- app.js
+++ app.js
@@ -17,8 +17,24 @@ io.on('connection', function(socket){
   console.log('a user connected');
   socket.on('choice', function(what){
     console.log('chosen: ' + what);
+
+    if(what === 'justin beiber')
+      votes['justin beiber']++;
+    else
+      votes['one direction']++;
+
+    console.log(votes);
+
+    socket.emit('total',votes);
+
   });
 });
+
+
+var votes = {
+  'justin beiber' : 0,
+  'one direction' : 0
+};
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
